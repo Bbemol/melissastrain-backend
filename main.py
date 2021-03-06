8b6cec33eb83b71169e09bd156943df987db8b34
Oscar Marion <contact@oscarmarion.fr>
2021-03-06 15:48:26 +0100

    fix: Wrap in __main__

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from services.sncf import get_stop_areas
 
-test = get_stop_areas(87113001)
+def main():
+    test = get_stop_areas(87113001)
+    print(test)
 
-print(test)
+if __name__ == '__main__':
+    main()
