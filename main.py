e8bd98927eac5f458f73e2b3e96d2a190c0f72f0
Oscar Marion <contact@oscarmarion.fr>
2021-03-06 19:34:05 +0100

    feat: list arrivals for all parisian stations

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from services.sncf import Station
+from services.sncf import Station, City
+
+from utilities.list import List
 
 def main():
-    paris_est = Station(87113001).get_arrivals()
-    print(paris_est)
+    # paris_est = Station(87113001).get_arrivals()
+    # print(paris_est)
+    result = []
+
+    paris_stations = City("Paris").get_stations()
+
+    for station in paris_stations:
+        name, id = List.filter(station, ["name", "id"]).values()
+        arrivals = Station(id).get_arrivals()
+        result.append({"name": name, "id": id, "arrivals": arrivals})
+
+    print(result)
 
 if __name__ == '__main__':
     main()
--- services/sncf.py
+++ services/sncf.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from requests.models import HTTPBasicAuth
 from utilities.env import Env
 
@@ -8,8 +9,8 @@ class SNCFService:
     token = Env.get("sncf-token")
 
     @staticmethod
-    def get(url):
-        response = requests.get(url, auth=HTTPBasicAuth(SNCFService.token, ''))
+    def get(url: str, params: set=None):
+        response = requests.get(url, params=params, auth=HTTPBasicAuth(SNCFService.token, ''))
         status_code = response.status_code
 
         if status_code != 200:
@@ -18,14 +19,36 @@ class SNCFService:
             return response.json()
 
 class Station:
-    def __init__(self, station_id):
-        self.station_id = station_id
+    def __init__(self, station_id: str):
+        self.station_id = Station.extract_id(station_id)
 
     @staticmethod
-    def create_query(station_id):
+    def extract_id(station_id: str):
+        regex = r"(.+:)(\d+)"
+        return re.search(regex, station_id).group(2)
+
+    @staticmethod
+    def create_query(station_id: int):
         return f"/stop_areas/stop_area:SNCF:{station_id}/arrivals"
 
     def get_arrivals(self):
         url = SNCF_ENDPOINT + Station.create_query(self.station_id)
 
         return SNCFService.get(url)
+
+class City:
+    def __init__(self, city: str):
+        self.city = city
+
+    @staticmethod
+    def filter_stop_areas(place):
+        return place["embedded_type"] == "stop_area"
+
+    def get_stations(self):
+        url = SNCF_ENDPOINT + "/places"
+        params = {"q": self.city}
+
+        places = SNCFService.get(url, params)["places"]
+        stop_areas = filter(City.filter_stop_areas, places)
+
+        return list(stop_areas)
